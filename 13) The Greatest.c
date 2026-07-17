0a4f028a2680713c29e909d7c3ca17492b0ecbe6
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-07-17 15:27:32 +0600

    Add files via upload

---
+++ 13) The Greatest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int A, B, C;
+    int maiorAB, maior;
+
+    scanf("%d %d %d", &A, &B, &C);
+
+    maiorAB = (A + B + abs(A - B)) / 2;
+    maior = (maiorAB + C + abs(maiorAB - C)) / 2;
+
+    printf("%d eh o maior\n", maior);
+
+    return 0;
+}
